fc833d32ebeed6da76f028667a516a25a5f57c4b
Keshav Mittal <keshav.mittal0401@gmail.com>
2023-02-21 22:55:25 +0530

    login-logout implemented

---
+++ user-authentication/src/main/java/com/authentication/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.authentication.controller;
+
+import com.authentication.entity.User;
+import com.authentication.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    private HashMap<String, String> convert(String res) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("response", res);
+        return map;
+    }
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/login")
+    public ResponseEntity<?> login(@RequestHeader("email") String email, @RequestHeader("password") String password) {
+        String response = userService.login(email, password);
+        if(response.equals("not-found") || response.equals("incorrect-passowrd")){
+            return ResponseEntity.badRequest().body(convert(response));
+        }
+        return ResponseEntity.accepted().body(convert(response));
+    }
+
+
+    @GetMapping("/logout")
+    public ResponseEntity<?> logout(@RequestHeader("email") String email) {
+        String response = userService.logout(email);
+        if(response.equals("not-found")){
+            return ResponseEntity.badRequest().body(convert(response));
+        }
+        return ResponseEntity.accepted().body(convert(response));
+    }
+}
