cc785cf8df7d396486985309b19172709fc00866
pratyush upadhyay <pratyush.upadhyay2013@gmail.com>
2023-04-13 23:55:31 +0530

    Added https and exception

--- patient-service/src/main/java/com/alibou/security/repository/PatientRepository.java
+++ patient-service/src/main/java/com/alibou/security/repository/PatientRepository.java
@@ -10,4 +10,7 @@ import org.springframework.stereotype.Repository;
 public interface PatientRepository extends JpaRepository<Patient, Integer> {
 
   Optional<Patient> findByEmail(String email);
+  boolean existsByEmail(String email);
+
+  boolean existsById(String id);
 }
